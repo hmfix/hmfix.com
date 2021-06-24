5907cec0c5d1dc98c54ec598cdbff71ac3b5d312
hmfix <86157218+hmfix@users.noreply.github.com>
2021-06-25 00:45:00 +0200

    Delete vulfocus-master/vulfocus-master directory

--- vulfocus-master/vulfocus-master/vulfocus-api/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-service nginx start
-wait
-service redis-server restart
-wait
-nohup python3 manage.py runserver 0.0.0.0:8000 &
-celery -B -A vulfocus worker -l info -E --logfile=celery.log
-
