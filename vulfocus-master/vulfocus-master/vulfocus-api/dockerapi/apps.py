cab2c712e64f8f0262c1efb5fca921cd5dda2583
hmfix <86157218+hmfix@users.noreply.github.com>
2021-06-25 01:10:10 +0200

    added new file

--- vulfocus-master/vulfocus-master/vulfocus-api/dockerapi/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DockerapiConfig(AppConfig):
-    name = 'dockerapi'
--- vulfocus-master/vulfocus-master/vulfocus-api/layout_image/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LayoutImageConfig(AppConfig):
-    name = 'layout_image'
